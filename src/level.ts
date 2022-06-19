c89dfefd32b3909bc5a2d6df85ff84ca4475e877
mikedegrassi <1006780@hr.nl>
2022-06-19 22:24:37 +0200

    Level kiezen (nog niet klaar) en een aantal toegevoegde foto's daarvoor

--- src/level.ts
+++ src/level.ts
@@ -1,57 +1,111 @@
 import * as PIXI from "pixi.js"
 import { Application } from "pixi.js"
-import { Assets } from './asset'
+import levelBG from "./images/levelBG.png"
+import worm from "./images/wormhench.png"
+import bossWorm from "./images/wormhench.png"
+import finn from "./images/FinnSword.png"
+import { Game } from "./game"
 
 export class Level {
 
     private _pixi: PIXI.Application
-    buttonDownSprite : PIXI.Texture = PIXI.Texture.from("croppedbuttonDown2")
+    
+    private loader: PIXI.Loader
 
-    background : PIXI.Texture
+    private backgroundSprite : PIXI.Sprite
+
+    finnSprite : PIXI.Sprite
+
+    button: PIXI.Sprite
+    
+    button2: PIXI.Sprite
+
     texture: PIXI.Texture<PIXI.Resource>
 
+    // public get pixi(): PIXI.Application { return this._pixi }
     constructor() {
+       
         PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
         this._pixi = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight })
-
         document.body.appendChild(this._pixi.view)
 
-        //window.location.href=App.ts
-        
-        let asset = new Assets(this)
-
-        //asset
+        // let asset = new Assets(this)
+        this.loader = new PIXI.Loader()
+        this.loader
+        .add("background", levelBG) 
+        .add("wormSprite", worm) 
+        .add("bossWormSprite", bossWorm)
+        .add("finnSprite", finn)
+        this.loader.load(() => this.doneLoading())
     }
 
-    public loadCompleted() {
-        let background : PIXI.Texture = PIXI.Texture.from("levelBG")
-        let backgroundSprite =  new PIXI.Sprite(background)
-        backgroundSprite.scale.set(0.5)
+    doneLoading(){
+        let background : PIXI.Texture = PIXI.Texture.from("background")
+        this.backgroundSprite =  new PIXI.Sprite(background)
+        this.backgroundSprite.scale.set(0.5)
+        this._pixi.stage.addChild(this.backgroundSprite)
+
+        let finn : PIXI.Texture = PIXI.Texture.from("finnSprite")
+        this.finnSprite = new PIXI.Sprite(finn)
+        this.finnSprite.scale.set(3.5)
+        this.finnSprite.x = 30
+        this.finnSprite.y = 295
+        this._pixi.stage.addChild(this.finnSprite)
+      
+
+        this.button = new PIXI.Sprite(this.loader.resources["wormSprite"].texture!)
+        this.button.anchor.set(0.5) 
+        this.button.x = 530
+        this.button.y = 463
+        this.button.angle = 8
+        this.button.scale.set(4)
+
+        this.button.interactive = true;
+        this.button.buttonMode = true;
+
+        this._pixi.stage.addChild(this.button)
     
-        this._pixi.stage.addChild(backgroundSprite)
+        this.button2 = new PIXI.Sprite(this.loader.resources["bossWormSprite"].texture!)
+        this.button2.anchor.set(0.5) 
+        this.button2.x = 1325    
+        this.button2.y = 600
+        this.button2.scale.set(6)
 
-        let playButton : PIXI.Texture = PIXI.Texture.from("croppedbutton2");
-        let playButtonDown : PIXI.Texture = PIXI.Texture.from("croppedbuttonDown2");
+        this.button2.interactive = true;
+        this.button2.buttonMode = true;
 
-        let playButtonSprite = new PIXI.Sprite(playButton)
-        playButtonSprite.anchor.set(0.5)
-        playButtonSprite.x = backgroundSprite.width * 0.5
-        playButtonSprite.y = backgroundSprite.height * 0.5
-        playButtonSprite.scale.set(5);
+        this._pixi.stage.addChild(this.button2)
+
+        this.button.on('mousedown', () => this.onButtonDown("one"))
+        this.button.on('mouseup', () => this.onButtonUp())
+
+        this.button2.on('mousedown', () => this.onButtonDown("two"))
+        this.button2.on('mouseup', () => this.onButtonUp())
+
+    
         
-        
-        playButtonSprite.on('pointerdown', this.onButtonDown);
 
-        this._pixi.stage.addChild(playButtonSprite)
     }
 
-    private onButtonDown() {    
+
+    onButtonDown(name : string) {
+
+        switch (name){
+            case "one":
+                this.button.destroy
+            case"two":
+            console.log(name)
+                this.backgroundSprite.destroy
+        }
         
-        this.texture
-        // this.texture = this.;
     }
 
+    onButtonUp() {
+        
+    }
 
+     
+    
 
 }
